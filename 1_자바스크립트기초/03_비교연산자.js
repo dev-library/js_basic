066975e8947c8a9bcd720f3513ce350d3bb1a9be
devrary <dev.rary@gmail.com>
2023-05-02 17:08:03 +0900

    자바스크립트 1일차

---
+++ 1_자바스크립트기초/03_비교연산자.js
@@ -0,0 +1,18 @@
+var a = 5;
+var b = '5';
+console.log(a === b);
+console.log(a !== b);
+console.log("=======================");
+
+// == 비교는 결과 예측대로 흘러가지 않는 경우가 많음.
+console.log('0' === '');// false
+console.log(0 === '');// false
+console.log('0' === 0);//false
+console.log(false === 'false');//false
+console.log(false === '0');//false
+
+// 문자열 대소비교
+console.log("--------------------------");
+console.log( 'ㄱ' > 'Be' );
+
+
