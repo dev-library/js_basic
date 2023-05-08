a82fb163bb97fea1a226aa265037bebaa8b49a17
devrary <dev.rary@gmail.com>
2023-05-08 09:41:18 +0900

    JS 함수까지 완료

---
+++ 4_함수/08_const.js
@@ -0,0 +1,16 @@
+const TODAY_SALE_RATE = 0.2;
+const PI = 3.14159265;
+
+const kim = {
+    name : '김철수',
+    age : 25,
+};
+
+//kim = {
+//    name : '새철수',
+//    age : 15,
+//};
+
+kim.name = '김칠수';
+
+console.log(kim);
